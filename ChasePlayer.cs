72650098b92a32f771069d6de0108c6c2b42d516
I_y <49327759+IshidaYutaka@users.noreply.github.com>
2020-09-04 05:30:46 +0900

    Add files via upload

---
+++ ChasePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(obj.transform.position);
+    }
+}
