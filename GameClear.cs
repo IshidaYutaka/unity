6852751c084d89b9b9a02848d3fe992ccee9aa68
I_y <49327759+IshidaYutaka@users.noreply.github.com>
2020-09-04 06:32:18 +0900

    Add files via upload

---
+++ GameClear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Text Text;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("a");
+        if (other.transform.CompareTag("Player"))
+        {
+            Text.text = "Game Clear";
+            Time.timeScale = 0;
+        }
+    }
+}
